5480f3384e3840b02cc0a636cdedad624f759981
pradeep <pradeepjangid.jky@gmail.com>
2020-03-07 15:57:40 +0530

    test aria2

---
+++ README.md
@@ -0,0 +1,1 @@
+# BAYMAX BOT
---
+++ index.py
@@ -0,0 +1,78 @@
+import requests
+import os
+import aria2p
+import subprocess
+from flask import Flask, render_template, request, redirect, make_response, session,url_for
+app = Flask(__name__)
+app.secret_key = '767rgdb263tr'
+
+#how to get environment varible values -->  " os.environ['S3_KEY'] "
+
+@app.route('/run',methods = ['GET'])
+def run():
+    aria2_daemon_start_cmd = []
+    aria2_daemon_start_cmd.append("aria2c")
+    aria2_daemon_start_cmd.append("--daemon=true")
+    aria2_daemon_start_cmd.append("--enable-rpc")
+    aria2_daemon_start_cmd.append("--follow-torrent=mem")
+    aria2_daemon_start_cmd.append("--max-connection-per-server=10")
+    aria2_daemon_start_cmd.append("--min-split-size=10M")
+    aria2_daemon_start_cmd.append("--rpc-listen-all=false")
+    aria2_daemon_start_cmd.append("--rpc-listen-port=6800")
+    aria2_daemon_start_cmd.append("--rpc-max-request-size=1024M")
+    aria2_daemon_start_cmd.append("--seed-ratio=0.0")
+    aria2_daemon_start_cmd.append("--seed-time=1")
+    aria2_daemon_start_cmd.append("--split=10")
+    aria2_daemon_start_cmd.append("--bt-stop-timeout=600")
+
+    subprocess.Popen(aria2_daemon_start_cmd)
+    subprocess.call
+    global aria2 = aria2p.API(
+        aria2p.Client(
+            host="http://localhost",
+            port=6800,
+            secret=""
+        )
+    )
+    # list downloads
+    downloads = aria2.get_downloads()
+    list1=[]
+    for download in downloads:
+        list1.append(download.name)
+        list1.append(download.download_speed)
+        list1.append("<br>")
+    return list1
+
+@app.route('/',methods = ['GET'])
+def home():
+    # list downloads
+    downloads = aria2.get_downloads()
+    list1=[]
+    for download in downloads:
+        list1.append(download.name)
+        list1.append(",")
+        list1.append(download.download_speed)
+        list1.append("<br>")
+    return list1
+
+@app.route('/download',methods = ['GET'])
+def download():
+    magnet_uri = request.args.get('link')
+    gid = aria2.add_magnet(magnet_uri).gid
+    new_gid=aria2.get_download(gid).followed_by_ids[0]
+    file = aria2.get_download(new_gid)
+    list2=[]
+    list2.append(str(file.name))
+    list2.append(",")
+    list2.append(str(file.download_speed_string()))
+    list2.append(",")
+    list2.append(str(file.upload_speed_string()))
+    list2.append(",")
+    list2.append(str(file.progress_string()))
+    list2.append(",")
+    list2.append(str(file.total_length_string()))
+    return list2
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
